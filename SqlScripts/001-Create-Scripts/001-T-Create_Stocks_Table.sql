946bed4b19d2017cc9f4e9fed81b4fda036c5ec1
Crystabel-Isioma <crystabelisiomaobiasili@gmail.com>
2025-12-07 19:10:12 +0100

    Enhance Stock table with constraints and index
    Added primary key, foreign key, nonclustered index, and check constraint to Stock table.

---
+++ SqlScripts/001-Create-Scripts/001-T-Create_Stocks_Table.sql
@@ -0,0 +1,40 @@
+/* ============================================================
+6.	CREATE Stock TABLE
+   ============================================================ */ 
+CREATE TABLE dbo.Stock(
+  product_id         INT       NOT NULL,
+  stock_level        INT       NOT NULL,
+  last_updated       DATE      NULL,
+
+
+  -- PK
+	CONSTRAINT [PK_Stock_product_id] PRIMARY KEY CLUSTERED 
+		([product_id] ASC)
+		WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) 
+		ON [PRIMARY],
+
+
+  
+  -- FK
+	CONSTRAINT [FK_Stock_Products] FOREIGN KEY([product_id])
+		REFERENCES [dbo].[Products] ([product_id]),
+
+
+  ------------------------------------------------------------------------------------
+--===============| Create Nonclustered Index for product_id |===============--
+
+CREATE NONCLUSTERED INDEX [IX_Stock_product_id] 
+	ON [dbo].[Stock]
+	([product_id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+
+  
+  --===============| Adding CHECK Constraints |===============--
+-- 'stock_level' : Must Not Be Negative
+--==========================================================--
+ 
+  ALTER TABLE dbo.Stock 
+  ADD CONSTRAINT CK_Stock_NonNeg CHECK (stock_level >= 0)
+GO
