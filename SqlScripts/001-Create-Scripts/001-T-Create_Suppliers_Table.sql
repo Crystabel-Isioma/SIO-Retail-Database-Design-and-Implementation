6b0a6aa1d619d9db1899dbb0c2bcac3ad433e77f
Crystabel-Isioma <crystabelisiomaobiasili@gmail.com>
2025-12-07 18:12:02 +0100

    Create Suppliers table with constraints and index
    Added primary key constraint and nonclustered index for supplier_id.

---
+++ SqlScripts/001-Create-Scripts/001-T-Create_Suppliers_Table.sql
@@ -0,0 +1,23 @@
+/* ============================================================
+3.	CREATE Suppliers TABLE
+   ============================================================ */
+CREATE TABLE dbo.[Suppliers](
+  supplier_id           INT           IDENTITY(401,1)     NOT NULL,
+  supplier_name         NVARCHAR(60)                      NOT NULL,
+  phone                 NVARCHAR(20)                      NULL,
+
+
+  -- PK
+	CONSTRAINT [PK_Suppliers_supplier_id] PRIMARY KEY CLUSTERED 
+		([supplier_id] ASC)
+		WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) 
+		ON [PRIMARY],
+
+------------------------------------------------------------------------------------
+--===============| Create Nonclustered Index for AuthorID  |===============--
+
+CREATE NONCLUSTERED INDEX [IX_Suppliers_supplier_id] 
+	ON [dbo].[Suppliers]
+	([supplier_id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
