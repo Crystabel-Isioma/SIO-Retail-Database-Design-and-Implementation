7b5d83d59f83f5d671463cd57db00c2aeca01855
Crystabel-Isioma <crystabelisiomaobiasili@gmail.com>
2025-12-07 18:24:58 +0100

    Create Category table with indexes and constraints
    Added primary key and nonclustered indexes for category_id and category_name.

---
+++ SqlScripts/001-Create-Scripts/001-T-Create_Category_Table.sql
@@ -0,0 +1,33 @@
+/* ============================================================
+4.	CREATE Category TABLE
+   ============================================================ */
+  
+CREATE TABLE dbo.[Category](
+  category_id       INT IDENTITY(501,1)       NOT NULL,
+  category_name     NVARCHAR(20)              NOT NULL,
+  description       NVARCHAR(100)              NULL,
+
+
+-- PK
+	CONSTRAINT [PK_Category_category_id] PRIMARY KEY CLUSTERED 
+		([category_id] ASC)
+		WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) 
+		ON [PRIMARY],
+
+------------------------------------------------------------------------------------
+--===============| Create Nonclustered Index for category_id |===============--
+
+CREATE NONCLUSTERED INDEX [IX_Category_category_id] 
+	ON [dbo].[Category]
+	([category_id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+
+------------------------------------------------------------------------------------
+--===============| Create Nonclustered Index for category_name |===============--
+
+CREATE NONCLUSTERED INDEX [IX_Category_category_name] 
+	ON [dbo].[Category]
+	([category_name] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
